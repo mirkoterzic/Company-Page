ee064015361cd1ad4b98bcd2ba7116cc081f4832
mirkoterzic <mirko.terzic2002@gmail.com>
2024-07-11 14:15:38 +0200

    added services section

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    card.addEventListener("mouseenter", () => {
+      card.classList.add("hover");
+    });
+
+    card.addEventListener("mouseleave", () => {
+      card.classList.remove("hover");
+    });
+  });
+});
